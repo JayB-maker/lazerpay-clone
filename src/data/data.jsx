a71994c425ea1322ba4368e8ef18261bc2701220
JayB <ajiboyeabiodun001@gmail.com>
2022-07-11 16:51:33 +0200

    first commit

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -24,116 +24,88 @@ export const navs = [{
 }, {
     nav: "USE CASES",
     navItems: [{
-        icon: "fa-solid fa-bag-shopping",
+        icon: "fa-solid fa-alien",
         navItem:"Creators",
         link: "/",
     }, {
-        icon: "fa-solid fa-hexagon-image",
+        icon: "fa-solid fa-signal",
         navItem:"Startups",
         link: "/",
     }, {
-        icon: "fa-solid fa-globe-stand",
+        icon: "fa-solid fa-sausage",
         navItem:"Saas",
         link: "/",
     }, {
-        icon: "fa-solid fa-link",
+        icon: "fa-solid fa-dumpster",
         navItem:"E-commerce",
         link: "/",
     }, {
-        icon: "fa-solid fa-eject",
+        icon: "fa-solid fa-user-tie",
         navItem:"Professionals",
         link: "/",
     }, ]
 }, {
     nav: "PRICING",
     navItems: [{
-        icon: "fa-solid fa-bag-shopping",
-        navItem:"Checkout",
-        link: "/",
-    }, {
-        icon: "fa-solid fa-hexagon-image",
-        navItem:"Lazer Token",
-        link: "/",
-    }, {
-        icon: "fa-solid fa-globe-stand",
-        navItem:"Earn",
-        link: "/",
-    }, {
-        icon: "fa-solid fa-link",
-        navItem:"Payment Links",
-        link: "/",
-    }, {
-        icon: "fa-solid fa-eject",
-        navItem:"Payment Buttons",
+        icon: "fa-solid fa-tag",
+        navItem:"Pricing",
         link: "/",
     }, ]
 }, {
-    nav: "PRODUCT",
+    nav: "FOR BUSINESSES",
     navItems: [{
+        icon: "fa-solid fa-layer-group",
+        navItem:"Why LazerPay",
+        link: "/",
+    }, {
+        icon: "fa-solid fa-arrow-turn-down-right",
+        navItem:"How it Works",
+        link: "/",
+    }, {
         icon: "fa-solid fa-bag-shopping",
-        navItem:"Checkout",
-        link: "/",
-    }, {
-        icon: "fa-solid fa-hexagon-image",
-        navItem:"Lazer Token",
-        link: "/",
-    }, {
-        icon: "fa-solid fa-globe-stand",
-        navItem:"Earn",
-        link: "/",
-    }, {
-        icon: "fa-solid fa-link",
-        navItem:"Payment Links",
-        link: "/",
-    }, {
-        icon: "fa-solid fa-eject",
-        navItem:"Payment Buttons",
+        navItem:"Checkout Demo",
         link: "/",
     }, ]
 }, {
-    nav: "PRODUCT",
+    nav: "FOR DEVELOPER",
     navItems: [{
-        icon: "fa-solid fa-bag-shopping",
-        navItem:"Checkout",
+        icon: "fa-solid fa-sun-bright",
+        navItem:"Quick Start",
         link: "/",
     }, {
-        icon: "fa-solid fa-hexagon-image",
-        navItem:"Lazer Token",
+        icon: "fa-solid fa-books",
+        navItem:"Libraries",
         link: "/",
     }, {
-        icon: "fa-solid fa-globe-stand",
-        navItem:"Earn",
-        link: "/",
-    }, {
-        icon: "fa-solid fa-link",
-        navItem:"Payment Links",
-        link: "/",
-    }, {
-        icon: "fa-solid fa-eject",
-        navItem:"Payment Buttons",
+        icon: "fa-solid fa-folder-open",
+        navItem:"API Documentation",
         link: "/",
     }, ]
 }, {
-    nav: "PRODUCT",
+    nav: "COMPANY",
     navItems: [{
-        icon: "fa-solid fa-bag-shopping",
-        navItem:"Checkout",
+        icon: "fa-solid fa-briefcase",
+        navItem:"About",
         link: "/",
     }, {
-        icon: "fa-solid fa-hexagon-image",
-        navItem:"Lazer Token",
+        icon: "fa-solid fa-envelope-dot",
+        navItem:"Contact",
         link: "/",
     }, {
-        icon: "fa-solid fa-globe-stand",
-        navItem:"Earn",
+        icon: "fa-solid fa-users-medical",
+        navItem:"Team",
         link: "/",
     }, {
-        icon: "fa-solid fa-link",
-        navItem:"Payment Links",
+        icon: "fa-solid fa-pen-line",
+        navItem:"Blog",
         link: "/",
     }, {
-        icon: "fa-solid fa-eject",
-        navItem:"Payment Buttons",
+        icon: "fa-solid fa-user-tie-hair",
+        navItem:"Careers",
+        link: "/",
+    }, {
+        icon: "fa-solid fa-address-card",
+        navItem:"Press",
         link: "/",
     }, ]
 }, ]
