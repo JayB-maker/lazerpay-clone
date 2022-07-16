b8c3b5a8992dae00cdc75d51f4675a99f678b4a0
JayB <ajiboyeabiodun001@gmail.com>
2022-07-16 03:08:56 +0200

    fifth commit

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import AcceptPayment from "../components/homePage/AcceptPayment";
-import AllInOne from "../components/homePage/AllInOne";
-import GetStarted from "../components/homePage/GetStarted";
+// import AcceptPayment from "../components/homePage/AcceptPayment";
+// import AllInOne from "../components/homePage/AllInOne";
+// import GetStarted from "../components/homePage/GetStarted";
 import GrowWithLazerPay from "../components/homePage/GrowWithLazerPay";
 import HeroSection from "../components/homePage/HeroSection";
 import PowerOfCrypto from "../components/homePage/PowerOfCrypto";
-import PowerYourApp from "../components/homePage/PowerYourApp";
-import WhoCanUseLazerPay from "../components/homePage/WhoCanUseLazerPay";
-import Footer from "../components/layout/Footer";
+// import PowerYourApp from "../components/homePage/PowerYourApp";
+// import WhoCanUseLazerPay from "../components/homePage/WhoCanUseLazerPay";
+// import Footer from "../components/layout/Footer";
 import Header from "../components/layout/Header";
 import { acceptPaymentDetails, growWithLazerPayDetails } from "../data/data";
 
