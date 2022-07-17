cd3af64dc947325555e3c12fae60ea25af144d86
JayB <ajiboyeabiodun001@gmail.com>
2022-07-18 00:38:27 +0200

    sixth commit

--- src/components/homePage/GrowWithLazerPay/index.jsx
+++ src/components/homePage/GrowWithLazerPay/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { growWithLazerPayDetails } from "../../../data/data";
 
 const GrowContainer = styled.div`
   display: flex;
