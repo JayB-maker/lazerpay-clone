4a1316b7a2eaaa7aadd852530e64af41447bbf10
JayB <ajiboyeabiodun001@gmail.com>
2022-07-18 00:37:19 +0200

    sixth commit

--- src/Theme.jsx
+++ src/Theme.jsx
@@ -1,22 +1,24 @@
-import { createGlobalStyle } from "styled-components"
+import { createGlobalStyle } from "styled-components";
 
 export const Light = {
-    primary: "#f4f3ee",
-    accent: "#125bc9",
-    black: "#000000",
-    white: "#ffffff",
-    headingBlack: "#0f172a",
-    textBlack: "#ie293b"
-}
+  primary: "#f4f3ee",
+  accent: "#125bc9",
+  black: "#000000",
+  white: "#ffffff",
+  background: "#f8fafc",
+  headingBlack: "#0f172a",
+  textBlack: "#ie293b",
+};
 
 export const Dark = {
-    primary: "#f4f3ee",
-    accent: "#125bc9",
-    black: "#ffffff",
-    white: "#000000",
-    headingBlack: "#0f172a",
-    textBlack: "#ie293b"
-}
+  primary: "#f4f3ee",
+  accent: "#125bc9",
+  black: "#ffffff",
+  white: "#000000",
+  background: "#f8fafc",
+  headingBlack: "#0f172a",
+  textBlack: "#ie293b",
+};
 
 export const GlobalStyle = createGlobalStyle`
 
