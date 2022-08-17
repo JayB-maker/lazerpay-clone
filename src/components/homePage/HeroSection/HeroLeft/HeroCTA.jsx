7ee8151b700de63431dc7398369a9c84f1314655
JayB <ajiboyeabiodun001@gmail.com>
2022-08-17 22:41:00 +0200

    New commit

--- src/components/homePage/HeroSection/HeroLeft/HeroCTA.jsx
+++ src/components/homePage/HeroSection/HeroLeft/HeroCTA.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { LinkText } from "../../../ui";
 
 const CTA = styled.div`
   display: flex;
